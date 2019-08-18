9c41901c242cab7d48d98196b4d9e9270c37384e
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-18 21:04:32 +0300

    generate board
    the board has been generated with tiles at the right position

---
+++ Match3_TestTask/Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+    [SerializeField] FieldSettings fieldSettings;
+
+    List<GameObject> tiles;
+    Vector2 tileSize;
+    float boardHeight;
+    float boardWidth;
+    float startPositionX;
+    float startPositionY;
+
+    private void Awake()
+    {
+        tiles = new List<GameObject>();
+        tiles = fieldSettings.GetTiles();
+        boardHeight = fieldSettings.GetHeight();
+        boardWidth = fieldSettings.GetWidth();
+        startPositionX = transform.position.x;
+        startPositionY = transform.position.y;
+        tileSize = fieldSettings.GetTileSize();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+        for (float i = 0; i < boardWidth; i++)
+        {
+            for (float y = 0; y < boardHeight; y++)
+            {
+                int nextTileIndex = Random.Range(0, tiles.Count-1);
+                GameObject newTile= Instantiate(tiles[nextTileIndex], new Vector3(startPositionX + (tileSize.x * i), startPositionY + (tileSize.y * y)),
+                    transform.rotation);
+                newTile.transform.parent = transform;
+            }
+        }
+    }
+
+}
---
+++ Match3_TestTask/Assets/Scripts/FieldSettings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New field", menuName ="Field Properties")]
+public class FieldSettings : ScriptableObject
+{
+    [SerializeField] int height;
+    [SerializeField] int width;
+    [SerializeField] List<GameObject> tiles;
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public List<GameObject> GetTiles()
+    {
+        return tiles;
+    }
+
+    public Vector2 GetTileSize()
+    {
+        if (tiles != null)
+        {
+            GameObject tile = tiles[0];
+            Vector2 size = tile.GetComponent<SpriteRenderer>().bounds.size;
+            return size;
+        }
+        return new Vector2();
+    }
+}
