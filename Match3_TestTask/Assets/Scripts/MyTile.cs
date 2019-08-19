445608958c94ced1f2fedd2be91dae2e91fd3eeb
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-19 23:11:36 +0300

    swap

---
+++ Match3_TestTask/Assets/Scripts/MyTile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTile : MonoBehaviour
+{
+    [SerializeField] Color selectedColor;
+
+    SpriteRenderer renderer=null;
+    static MyTile previousTile = null;// to be able to see selected tile from previousScripts
+    bool isSelected=false;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (isSelected)
+        {
+            renderer.color = Color.white;
+            isSelected = false;
+            previousTile = null;
+        }
+        else
+        {
+            if (previousTile == null)
+            {
+                renderer.color = selectedColor;
+                isSelected = true;
+                previousTile = gameObject.GetComponent<MyTile>();
+            }
+            else
+            {
+                Swap(previousTile.renderer);
+                previousTile.renderer.color = Color.white;
+                previousTile = null;
+                isSelected = false;
+            }
+        }
+    }
+
+    private void Swap(SpriteRenderer tileRenderer)
+    {
+        if (tileRenderer.sprite == renderer.sprite)
+            return;
+
+        Sprite middleSprite = tileRenderer.sprite;
+        tileRenderer.sprite = renderer.sprite;
+        renderer.sprite = middleSprite;
+    }
+}
